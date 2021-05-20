dbcca620bd710c087532c8100eb853d4b8194ddc
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2021-05-20 09:24:29 -0300

    criacao do projeto

---
+++ src/test/kotlin/br/com/vfs/casadocodigoapi/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.vfs.casadocodigoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
