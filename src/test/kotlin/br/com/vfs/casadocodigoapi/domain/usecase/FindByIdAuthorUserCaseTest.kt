c0634e54af016989fa80c8ca37f3820c19c119fa
erickndrade <erickndradez@gmail.com>
2022-02-08 14:54:58 -0300

    teste de id

---
+++ src/test/kotlin/br/com/vfs/casadocodigoapi/domain/usecase/FindByIdAuthorUserCaseTest.kt
@@ -0,0 +1,53 @@
+package br.com.vfs.casadocodigoapi.domain.usecase
+
+import br.com.vfs.casadocodigoapi.domain.expection.AuthorDoNotExistException
+import br.com.vfs.casadocodigoapi.domain.expection.AuthorExistsInSystemException
+import br.com.vfs.casadocodigoapi.domain.gateway.AuthorDataGateway
+import br.com.vfs.casadocodigoapi.domain.model.Author
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import java.time.LocalDateTime
+
+@ExtendWith(MockitoExtension::class)
+class FindByIdAuthorUserCaseTest {
+
+    @Mock
+    lateinit var authorDataGateway: AuthorDataGateway
+
+    @InjectMocks
+    lateinit var findByIdAuthorUserCaseImpl: FindByIdAuthorUserCaseImpl
+
+    @Test
+    fun `should throw AuthorDoNotExistException when author's id don't exists in system`() {
+        val id = 2L
+        Mockito.`when`(authorDataGateway.findById(eq(id))).thenReturn(null)
+        Assertions.assertThrows(AuthorDoNotExistException::class.java) {
+            findByIdAuthorUserCaseImpl.execute(id)
+        }
+    }
+
+
+
+    @Test
+    fun `should return author when author's id exist in system`() {
+        val author = buildAuthor()
+        val id = 1L
+        Mockito.`when`(authorDataGateway.findById(eq(id))).thenReturn(author)
+        Assertions.assertDoesNotThrow {
+            val findAuthorById: Author = findByIdAuthorUserCaseImpl.execute(id)
+            Assertions.assertEquals(author, findAuthorById)
+        }
+    }
+
+    private fun buildAuthor() =
+        Author(
+            1, "batata@email", "Erick", "aventura", LocalDateTime.now()
+        )
+}
